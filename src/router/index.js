1207dfbdb47d83e766393262082444d753293fe8
Allen Royston <allen.royston@sky.uk>
2017-10-13 11:40:02 +0100

    cleanup

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+// import vue and vue modules
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// import components
 import MainHeader from '../components/MainHeader.vue'
 import Navigation from '../components/Navigation.vue'
 import Home from '../components/Home.vue'
@@ -8,13 +10,16 @@ import About from '../components/About.vue'
 import MainFooter from '../components/MainFooter.vue'
 import Card from '../components/Card.vue'
 
-Vue.use(Router)
+// make components available to use in other components
 Vue.component('Navigation', require('../components/Navigation.vue'))
 Vue.component('MainHeader', require('../components/MainHeader.vue'))
 Vue.component('MainFooter', require('../components/MainFooter.vue'))
 Vue.component('Card', require('../components/Card.vue'))
 
+// start vue
+Vue.use(Router)
 
+// set routes
 export default new Router({
   routes: [
     {
